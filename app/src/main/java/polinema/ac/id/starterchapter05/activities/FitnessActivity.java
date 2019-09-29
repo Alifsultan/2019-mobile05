675155cddfc86f773607380786033b73a479b026
alifsultan <alifsultan22@gmail.com>
2019-09-29 19:52:50 +0700

    Membuat Dynamic,Animasi,Tugas Fitnes

---
+++ app/src/main/java/polinema/ac/id/starterchapter05/activities/FitnessActivity.java
@@ -0,0 +1,27 @@
+package polinema.ac.id.starterchapter05.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+import polinema.ac.id.starterchapter05.R;
+
+public class FitnessActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fitness);
+
+    }
+
+    public void handleDips(View view) {
+
+    }
+
+    public void handlePushup(View view) {
+    }
+
+    public void handleHandstand(View view) {
+    }
+}
